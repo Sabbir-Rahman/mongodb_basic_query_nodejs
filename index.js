506e2ed514b9a32f52147375981261da564a6763
Sabbir <sabbirrahman42@iut-dhaka.edu>
2021-06-01 23:05:06 +0600

    find data

--- index.js
+++ index.js
@@ -5,13 +5,27 @@ const connectToMongoDb = async () => {
     await mongo().then( async(mongoose) => {
         try{
             console.log('Connected to mongodb!')
+            
+            //Add user
+            // const user = {
+            //     email: 'test@gmail.com',
+            //     username: 'Bob',
+            //     password: '123456'
+            // }
+            // await new userSchema(user).save()
 
-            const user = {
-                email: 'test@gmail.com',
-                username: 'Sabbir Required',
-                password: '123456'
-            }
-            await new userSchema(user).save()
+            //find all user
+            const result = await userSchema.find()
+
+            //find user with user name
+            //findOne will return one that first match it is faster than the find
+            const result = await userSchema.find({
+                username: 'Sabbir'
+            })
+
+            console.log('Result:', result)
+
+
             
         } finally{
             mongoose.connection.close()
